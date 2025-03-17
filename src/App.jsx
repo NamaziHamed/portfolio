0eb914b1688720dee5d1c8c019123ab21cb486f9
Hamed Namazi <H.namazi91@gmail.com>
2025-03-17 19:58:08 +0330

    base name loading implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
+  state = {
+    status: "nameLoad",
+  };
+  name = ["H", "a", "m", "e", "d", " ", "N", "a", "m", "a", "z", "i"];
 
-  render() { 
+  render() {
     return (
-      <h1>Hamed Namazi</h1>
+      <>
+        {this.state.status === "nameLoad" && (
+          <p>
+            {this.name.map((char, index) => (
+              <span
+                key={index}
+                style={{
+                  animation: `fadeIn 0.3s ease ${index * 0.15}s forwards`,
+                  opacity: 0, // Default to hidden
+                }}
+              >
+                {char}
+              </span>
+            ))}
+          </p>
+        )}
+      </>
     );
   }
 }
- 
+
 export default App;
