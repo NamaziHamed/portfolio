47c9bde21b3ae6838e20dc7b2338178eacbb1a3c
Hamed Namazi <H.namazi91@gmail.com>
2025-03-19 20:49:16 +0330

    fade size, style, and timing correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,38 @@ class App extends Component {
   state = {
     status: "nameLoad",
   };
+
   name = ["H", "a", "m", "e", "d", " ", "N", "a", "m", "a", "z", "i"];
+
+componentDidMount = ()=>{
+  setTimeout(()=>{
+    this.setState({status: "nameLoaded"})
+  }, ((this.name.length * 150) + 1000) )
+}
 
   render() {
     return (
       <>
         {this.state.status === "nameLoad" && (
-          <p>
+          <h1 style={{
+            width: '100%',
+            height: '98vh',
+            textAlign: 'center',
+            transform: 'translateY(40%)',
+            fontSize: "7vw"
+          }}>
             {this.name.map((char, index) => (
               <span
                 key={index}
                 style={{
                   animation: `fadeIn 0.3s ease ${index * 0.15}s forwards`,
-                  opacity: 0, // Default to hidden
+                  opacity: 0,
                 }}
               >
                 {char}
               </span>
             ))}
-          </p>
+          </h1>
         )}
       </>
     );
